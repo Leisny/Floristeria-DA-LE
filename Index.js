87ece54f426d7df345f1ab1e84dad5da38d3fb3c
davinsonpympy <davinson.palacios.1306@miremington.edu.co>
2023-11-19 18:48:07 -0500

    imagenes js

---
+++ Index.js
@@ -0,0 +1,35 @@
+/*Imagen de Facebook*/
+var contenedor = document.getElementById('redes-sociales');
+var enlace = document.createElement("a");
+enlace.href = 'https://es-la.facebook.com/PLANTAS.FLORES/';
+
+var imagen = document.createElement("img");
+imagen.src = '/Imagenes/facebook.png';
+imagen.alt = "Descripción de la imagen";
+
+enlace.appendChild(imagen);
+contenedor.appendChild(enlace);
+
+/*Imagen de Instagram*/
+var contenedor = document.getElementById('redes-sociales');
+var enlace = document.createElement("a");
+enlace.href = 'https://www.instagram.com/florescolombia.co/';
+
+var imagen = document.createElement("img");
+imagen.src = '/Imagenes/instagram.png';
+imagen.alt = "Descripción de la imagen";
+
+enlace.appendChild(imagen);
+contenedor.appendChild(enlace);
+
+/*Imagen de Twitter*/
+var contenedor = document.getElementById('redes-sociales');
+var enlace = document.createElement("a");
+enlace.href = ' https://twitter.com/floresvirtual';
+
+var imagen = document.createElement("img");
+imagen.src = '/Imagenes/twitter.png';
+imagen.alt = "Descripción de la imagen";
+
+enlace.appendChild(imagen);
+contenedor.appendChild(enlace);
